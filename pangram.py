37184f08cb70ebe7d4ff1f117dde38ef69b2119a
Garry <32338075+Dingosnow@users.noreply.github.com>
2018-12-10 17:23:29 +0000

    Initial commit

---
+++ pangram.py
@@ -0,0 +1,247 @@
+#def hell(word, x):
+
+        #Create a list of alphabet
+#class MyLetters:
+  #def __iter__(self):
+   # self.
+
+  
+t = 25
+def = input {"Enter:"}:
+
+asat = {
+  '0':'a', '1':'b', '2':'c', '3':"d", '4':"e", '5':"f", '6':"g", '7':"h", '8':"i", '9':"j", '10':"k",
+  '11':"l", '12':"m", '13':"n", '14':"o", '15':"p", '16':"q", '17':"r", '18':"s", '19':"t",'20':"u",
+  '21':"v", '22':"w", '23':"x", '24':"y", '25':"z"
+}
+
+result = [character for character in pangram]
+print (result)
+
+for value in sorted(asat.items()):
+  if value in pangram:
+    print (value)
+
+#while t > 0:
+ # if key[] in asat.items():
+  #  print ("Y") #asat.values()[0].keys()[0]
+
+#for in dict.items():
+#print ("asat['0']: ")
+
+#for character in result():
+ # for i in result:
+  #  if i==k:
+   #   print ("yes")
+
+#for i in asat.keys() & set(result):
+ # print ("yes")
+
+#print ("no")
+
+#n = {}
+#for k,v in set.items():
+ #   for i in lis:
+  #      if i==k:
+   #         n[k] = v
+
+#x = len (asat)
+#len (asat)
+#print (x)
+#i= ("a")
+#asat.remove ()
+#x = len (asat)
+#print (x)
+
+#while t > 1:
+ # if asat1 == i:
+  #  asat.remove (00)
+   # t = t-1
+    #print ("Done!")
+    
+    
+#a = ("a","b","c","d","e","f","g","h","i","j","k",
+ #          "l","m","n","o","p","q","r","s","t","u",
+  #         "v","w","x","y","z")
+#x = input("Please enter a phrase:")
+#i = 1
+
+#while i < 24:
+ # if a (0:0) in x:
+  #  remove a (0:0)
+  #i = i+1
+  #print (i)
+
+
+
+    #return self
+
+  #def __next__(self):
+   # if self.a = (0):
+    #  x = self.a
+     # self.a += 1
+      #return x
+    #else:
+     # raise StopIteration
+
+#myclass = MyLetters ()
+#myiter = iter(myclass)
+
+#for x in myiter:
+ # print(x)
+  
+#Val1 = ()
+
+
+
+
+
+
+#x = input ("Enter:")
+#x = x.__iter__()
+#myit = iter(Val1)
+#myit2 = x
+
+
+  #myit2 = iter(Val2)
+#if x in Val1:
+ # print (x)
+  #x = (next(Val1))
+  #print (x)
+#print (x)
+#pass
+
+#while myit<24:
+ # if myit in myit:
+ #   print (myit)
+
+#print ("done")
+
+
+
+
+
+
+
+  #print(next(myit))
+  #print(next(myit))
+  #print(next(myit))
+
+        #Request user input to be checked against list
+#x = input ("Enter:")
+
+#for x.firstLetter in list:
+ # if x.firstLetter in list:
+  #  print (x)
+
+#hell ("","")
+
+
+           
+  #i = list [0]
+
+
+  # for x in list:
+  # print (list [0])
+#   dict(0) =x
+#print(x)
+#list.remove (x)
+#print (len(list))
+  #print (list [0])
+  #if (len(list)) > 1:
+  #hell(list, [0])
+  #print (list[0])
+  #if word == []:
+  #print ("hello")
+  #else:
+  #print ("goodbye")
+
+
+  
+#print_with_exclamation("spam")
+#print_with_exclamation("eggs")
+#print_with_exclamation("python")
+
+ # help.t = 2
+ # help.input = "Hello"
+  #while help.t < 2:
+   #if "Hello" in help.input:
+    #print ("yes")
+  #t = t+1
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def is_pangram(sentence):
+#n = 1
+#text = ("b")
+#dict =["abcdefghijklmnopqrstuvwxyt"]
+#def ncheck (n,result):
+ #     ncheck (0,True)
+  #    while n < 2:
+   #     if text in dict:
+    #     n = n+1
+     # print ("almost")
+
+
+#n = "0Why did I make this so hard"
+#Word ="abcdefghijklmnopqrstuvwxyz"
+#if n == "Why did I make this so hard":
+ #   print('Success!')
+
+#String firstLetter = result.Text[0].ToString()
+#if (result.Text.Length == 18)
+
+
+#n = 1
+#hell = ("b")
+#dict =["abcdefghijklmnopqrstuvwxyt"]
+#def ncheck (n,result):
+ #     if string.find(ncheck):
+  #       print ('success')
+
+#phrase = ("I can do this")
+#dict = ["a", "b", "f"]
+
+#def alphaCheck():
+ #  i = 0
+  # while i <1:
+   #   if i in dict:
+    #    print ("i")
+     #   i = i+1
+   #else:
+    #  if i not in dict:
+     #    print ("done")
+
+
+           #print ("no")
+
+    #for char in dict:
+      # iterator.next()
+
+#pass
